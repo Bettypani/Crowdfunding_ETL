f88671a94e0f22beac19bf4eca868108d8ae5ee2
Bettypani <153875882+Bettypani@users.noreply.github.com>
2024-03-21 22:04:43 -0400

    Update crowdfunding_db_schema.sql

--- Crowdfunding_ETL/ETL_Mini_Project_DCabrera_BPaniagua.ipynb/crowdfunding_db_schema.sql
+++ Crowdfunding_ETL/ETL_Mini_Project_DCabrera_BPaniagua.ipynb/crowdfunding_db_schema.sql
@@ -1,37 +1,46 @@
-﻿
+--Drop tables if necessary
+--DROP TABLE categories,
+--	subcategories,
+--	contacts,
+--	campaign;
 CREATE TABLE categories(
-    category_id        VARCHAR(30)      NOT NULL    PRIMARY KEY,
+    category_id        VARCHAR(4)      NOT NULL    PRIMARY KEY,
     category           VARCHAR(30)     NOT NULL
 );
-
 CREATE TABLE subcategories(
-    subcategory_id     VARCHAR(30)     NOT NULL   PRIMARY KEY,
+    subcategory_id     VARCHAR(30)     NOT NULL    PRIMARY KEY,
     subcategory        VARCHAR(30)      NOT NULL
 );
-
 CREATE TABLE contacts(
     contact_id        INTEGER          NOT NULL    PRIMARY KEY,
     fist_name         VARCHAR(30)      NOT NULL,
     last_name         VARCHAR(30)      NOT NULL,
     email             VARCHAR(60)      NOT NULL
 );
-
 CREATE TABLE campaign(
     cf_id           INTEGER           NOT NULL    PRIMARY KEY,
     contact_id      INTEGER           NOT NULL,
-    company_name    VARCHAR(40)       NOT NULL,
-    description     VARCHAR(40)       NOT NULL,
-    goal            INTEGER           NOT NULL,
-    pledged         INTEGER           NOT NULL,
-    outcome         VARCHAR(30)      NOT NULL,
+    company_name    VARCHAR(60)       NOT NULL,
+    description     VARCHAR(60)       NOT NULL,
+    goal            FLOAT           NOT NULL,
+    pledged         FLOAT           NOT NULL,
+    outcome         VARCHAR(60)      NOT NULL,
     backers_count   INTEGER           NOT NULL,
-    country         VARCHAR(30)      NOT NULL,
-    currency        VARCHAR(30)      NOT NULL,
+    country         VARCHAR(60)      NOT NULL,
+    currency        VARCHAR(60)      NOT NULL,
     launched_date   DATE             NOT NULL,
     end_date        DATE             NOT NULL,
-    category_id     VARCHAR(30)       NOT NULL,
-    subcategory_id  VARCHAR(30)   NOT NULL,
+    category_id     VARCHAR(4)       NOT NULL,
+    subcategory_id  VARCHAR(60)   NOT NULL,
     FOREIGN KEY(category_id)  REFERENCES categories(category_id),
     FOREIGN KEY(subcategory_id)  REFERENCES subcategories(subcategory_id),
     FOREIGN KEY(contact_id)  REFERENCES contacts(contact_id)
 )
+SELECT *
+FROM categories;
+SELECT *
+FROM subcategories;
+SELECT *
+FROM contacts;
+SELECT *
+FROM campaign;
